3194d100ff270b46a8e475bb560f7000e189f914
NikiGamePlay <nikigameplay@outlook.com>
2019-03-25 19:45:15 +0100

    adding more comments

--- source/kernel.c
+++ source/kernel.c
@@ -4,8 +4,8 @@ execution of the kernel begins and ends here
 #include "include/screen.h"
 #include "include/charcolors.h"
 
-const char *kernelName = "Ultranium Kernel";
-const char *kernelVersion = "v0.01 Alpha";
+const char *kernelName = "Ultranium Kernel"; // define kernel name
+const char *kernelVersion = "v0.01 Alpha"; // define kernel version
 
 void kernel_exit_handler(void) {
     const char *bracket1 = "[";
@@ -27,11 +27,12 @@ void kmain(void)
 {
 	clear_screen();
     kprint(kernelName);
+    kprint(" "); // space between kernel name and version
     kprint(kernelVersion);
     kprint_newline();
     /*
     TODO: Add functionality
     */
-    kernel_exit_handler();
+    kernel_exit_handler(); // call on-exit procedure
 	return;
 }
--- source/screen.c
+++ source/screen.c
@@ -1,33 +1,45 @@
 #include "include/screen.h"
 #include "include/charcolors.h"
 
-char *vidmemptr = (char*)0xb8000;
-unsigned int current_loc = 0;
+char *vidmemptr = (char*)0xb8000; // pointer to video memory
+unsigned int current_loc = 0; // current char location
 
+/*
+Writes chars to video memory
+*/
 void kprint(const char *str)
 {
 	unsigned int i = 0;
-	while (str[i] != '\0') {
-		vidmemptr[current_loc++] = str[i++];
+	while (str[i] != '\0') { // print until null-byte is present
+		vidmemptr[current_loc++] = str[i++]; // print char
 		vidmemptr[current_loc++] = CHAR_FG_LIGHTGRAY; // light gray font
 	}
 }
 
+/*
+kprint() variant with colored text
+*/
 void kprintcolored(const char *str, unsigned int color)
 {
 	unsigned int i = 0;
-	while (str[i] != '\0') {
-		vidmemptr[current_loc++] = str[i++];
-		vidmemptr[current_loc++] = color; // light gray font
+	while (str[i] != '\0') { // print until null-byte is present
+		vidmemptr[current_loc++] = str[i++]; // print char
+		vidmemptr[current_loc++] = color; // set attrib-byte to user-defined color
 	}
 }
 
+/*
+Print empty chars until the end of current line
+*/
 void kprint_newline(void)
 {
 	unsigned int line_size = CHAR_BYTE_SIZE * COLUMNS_IN_LINE;
 	current_loc = current_loc + (line_size - current_loc % (line_size));
 }
 
+/*
+Loop over the entire screen, filling video memory with empty chars
+*/
 void clear_screen(void)
 {
 	unsigned int i = 0;
