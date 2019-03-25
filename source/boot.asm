0737097c2cefeb90859e7f64eeb12e815f29e0ae
NikiGamePlay <nikigameplay@outlook.com>
2019-03-25 19:39:23 +0100

    small changes in boot.asm

--- source/boot.asm
+++ source/boot.asm
@@ -1,23 +1,19 @@
-;;kernel.asm
-
-;nasm directive - 32 bit
-bits 32
+bits 32 ;; 32-bit
 section .text
-        ;multiboot spec
         align 4
-        dd 0x1BADB002            ;magic
+        dd 0x1BADB002            ;magic value
         dd 0x00                  ;flags
         dd - (0x1BADB002 + 0x00) ;checksum. m+f+c should be zero
 
 global start
-extern kmain	        ;kmain is defined in the c file
+extern kmain
 
 start:
-  cli 			;block interrupts
-  mov esp, stack_space	;set stack pointer
+  cli 			; disable interrupts
+  mov esp, stack_space	; set stack pointer
   call kmain
-  hlt		 	;halt the CPU
+  hlt		 	; disable CPU after kernel exit
 
 section .bss
-resb 8192		;8KB for stack
+resb 8192		; 8KB of stack space
 stack_space:
